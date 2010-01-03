f1fd686c12d915cea5439214de93129c3cb944c9
Tony Arnold <tony@tonyarnold.com>
2010-01-03 19:23:44 +1100

    Formatting fixes, and changed from countach.

--- Color Label Button/ColorLabelButtonCell.h
+++ Color Label Button/ColorLabelButtonCell.h
@@ -1,47 +1,47 @@
 //
-//  ColorLabelButtonCell.h
-//  Color Label Button
+// ColorLabelButtonCell.h
+// Color Label Button
 //
-//  Created by Tony Arnold on 20/12/06.
-//  Some rights reserved: <http://creativecommons.org/licenses/by/2.5/>
+// Created by Tony Arnold on 20/12/06.
+// Some rights reserved: <http://creativecommons.org/licenses/by/2.5/>
 //
 
 #import <Cocoa/Cocoa.h>
 
 typedef enum {
-	ClearLabelType	= 0, 
-	ColorLabelType	= 1, 
-} ColorLabelButtonType; 
+	ClearLabelType = 0,
+	ColorLabelType = 1,
+} ColorLabelButtonType;
 
 @interface ColorLabelButtonCell : NSActionCell {
-  NSColor*	mColor;
-	NSImage*	mImage;
-	
-	ColorLabelButtonType	mType;
-	
-	BOOL		mMouseInside;
-	BOOL		mSelected;
+	NSColor *mColor;
+	NSImage *mImage;
+
+	ColorLabelButtonType mType;
+
+	BOOL mMouseInside;
+	BOOL mSelected;
 }
 
 #pragma mark -
-- (id) init; 
-- (void) dealloc; 
+- (id)init;
+- (void)dealloc;
 
 #pragma mark -
-#pragma mark Attributes 
-- (void) setColor: (NSColor*) color; 
-- (NSColor*) color; 
+#pragma mark Attributes
+- (void)setColor:(NSColor *)color;
+- (NSColor *)color;
 
 #pragma mark -
-- (void) setLabelType: (ColorLabelButtonType) type; 
-- (ColorLabelButtonType) labelType; 
+- (void)setLabelType:(ColorLabelButtonType)type;
+- (ColorLabelButtonType)labelType;
 
 #pragma mark -
-- (void) setSelected: (BOOL) flag; 
-- (BOOL) selected; 
+- (void)setSelected:(BOOL)flag;
+- (BOOL)selected;
 
 #pragma mark -
-- (void) mouseEntered: (NSEvent*) event;
-- (void) mouseExited: (NSEvent*) event;
+- (void)mouseEntered:(NSEvent *)event;
+- (void)mouseExited:(NSEvent *)event;
 
 @end
--- Color Label Button/Color_Label_ButtonInspector.m
+++ Color Label Button/Color_Label_ButtonInspector.m
@@ -1,9 +1,9 @@
 //
-//  Color_Label_ButtonInspector.m
-//  Color Label Button
+// Color_Label_ButtonInspector.m
+// Color Label Button
 //
-//  Created by Tony Arnold on 20/12/06.
-//  Some rights reserved: <http://creativecommons.org/licenses/by/2.5/>
+// Created by Tony Arnold on 20/12/06.
+// Some rights reserved: <http://creativecommons.org/licenses/by/2.5/>
 //
 
 #import "Color_Label_ButtonInspector.h"
@@ -11,23 +11,20 @@
 
 @implementation Color_Label_ButtonInspector
 
-- (id)init
-{
-    self = [super init];
-    [NSBundle loadNibNamed:@"Color_Label_ButtonInspector" owner:self];
-    return self;
+- (id)init {
+	self = [super init];
+	[NSBundle loadNibNamed:@"Color_Label_ButtonInspector" owner:self];
+	return self;
 }
 
-- (void)ok:(id)sender
-{
+- (void)ok:(id)sender {
 	/* Your code Here */
-    [super ok:sender];
+	[super ok:sender];
 }
 
-- (void)revert:(id)sender
-{
+- (void)revert:(id)sender {
 	/* Your code Here */
-    [super revert:sender];
+	[super revert:sender];
 }
 
 @end

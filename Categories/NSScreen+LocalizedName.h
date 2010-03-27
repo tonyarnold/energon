96acf8d371c1e56d977cfd4bcd9e730d2740c923
Tony Arnold <tony@tonyarnold.com>
2010-03-28 10:47:29 +1100

    A category for NSScreen that returns the proper localised name of a display.

---
+++ Categories/NSScreen+LocalizedName.h
@@ -0,0 +1,17 @@
+/*
+ *  NSScreen+LocalizedName.h
+ *  Created as part of Hyperspaces - http://thecocoabots.com/hyperspaces/
+ *
+ *  Created by Tony Arnold on 8/01/10.
+ *  Copyright 2010 The CocoaBots. All rights reserved.
+ *
+ */
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSScreen (LocalizedName)
+
+@property (readonly) NSString *localizedDisplayName;
+
+@end
---
+++ Categories/NSScreen+LocalizedName.m
@@ -0,0 +1,49 @@
+/*
+ *  NSScreen+LocalizedName.m
+ *  Created as part of Hyperspaces - http://thecocoabots.com/hyperspaces/
+ *
+ *  Created by Tony Arnold on 8/01/10.
+ *  Copyright 2010 The CocoaBots. All rights reserved.
+ *
+ */
+
+#import "NSScreen+LocalizedName.h"
+#import <IOKit/IOKitLib.h>
+#import <IOKit/graphics/IOGraphicsLib.h>
+
+@implementation NSScreen (LocalizedName)
+
+static void KeyArrayCallback(const void *key, const void *value, void *context) {
+	CFArrayAppendValue(context, key);
+}
+
+@dynamic localizedDisplayName;
+- (NSString *)localizedDisplayName {
+	NSDictionary     *screenDictionary = [self deviceDescription];
+	NSNumber         *screenID         = [screenDictionary objectForKey:@"NSScreenNumber"];
+	CGDirectDisplayID aID              = [screenID unsignedIntValue];
+	CFStringRef       localName        = NULL;
+	io_connect_t      displayPort      = CGDisplayIOServicePort(aID);
+	CFDictionaryRef   dict             = (CFDictionaryRef)IODisplayCreateInfoDictionary(displayPort, 0);
+	CFDictionaryRef   names            = CFDictionaryGetValue(dict, CFSTR(kDisplayProductName));
+
+	if (names) {
+		CFArrayRef langKeys = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
+		CFDictionaryApplyFunction(names, KeyArrayCallback, (void *)langKeys);
+		CFArrayRef orderLangKeys = CFBundleCopyPreferredLocalizationsFromArray(langKeys);
+		CFRelease(langKeys);
+
+		if (orderLangKeys && CFArrayGetCount(orderLangKeys)) {
+			CFStringRef langKey = CFArrayGetValueAtIndex(orderLangKeys, 0);
+			localName = CFDictionaryGetValue(names, langKey);
+			CFRetain(localName);
+		}
+
+		CFRelease(orderLangKeys);
+	}
+
+	CFRelease(dict);
+	return [(NSString *)localName autorelease];
+}
+
+@end

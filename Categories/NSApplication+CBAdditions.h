9ca9a29a0fd22ba5060c6502c51d8ecb39b49ef1
Tony Arnold <tony@thecocoabots.com>
2011-04-22 23:54:42 +1000

    Updating to use a whole lot less carbon for Mac OS X version finding methods.

--- Categories/NSApplication+CBAdditions.h
+++ Categories/NSApplication+CBAdditions.h
@@ -1,14 +1,18 @@
-/*
- *  NSApplication+CBAdditions.h
- *  Created as part of Hyperspaces - http://thecocoabots.com/hyperspaces/
- *
- *  Created by Tony Arnold on 30/06/09.
- *  Copyright 2009 The CocoaBots. All rights reserved.
- *
- */
+//
+// NSApplication+CBAdditions.h
+// Hyperspaces
+//
+// Created by Tony Arnold on 30/06/09.
+// Copyright 2009 The CocoaBots. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
 
+#ifndef NSAppKitVersionNumber10_6
+#define NSAppKitVersionNumber10_6 1038
+#endif
+
+extern NSString *const kShowDockIconUserDefaultsKey;
 
 @interface NSApplication (CBAdditions)
 
@@ -16,9 +20,15 @@
 - (BOOL)showsDockIcon;
 - (void)setShowsDockIcon:(BOOL)flag;
 - (void)relaunch;
-+ (void)getSystemVersionMajor:(unsigned *)major minor:(unsigned *)minor bugFix:(unsigned *)bugFix;
++ (BOOL)systemIsLeopard;
++ (BOOL)systemIsLeopardOrHigher;
++ (BOOL)systemIsSnowLeopard;
++ (BOOL)systemIsSnowLeopardOrHigher;
++ (BOOL)systemIsLion;
+- (NSString *)applicationCopyrightInformation;
 - (NSString *)applicationName;
 - (NSString *)applicationShortVersionString;
 - (NSString *)applicationVersion;
+- (NSString *)applicationIdentifier;
 
 @end
--- Categories/NSApplication+CBAdditions.m
+++ Categories/NSApplication+CBAdditions.m
@@ -1,129 +1,94 @@
-/*
- *  NSApplication+CBAdditions.m
- *  Created as part of Hyperspaces - http://thecocoabots.com/hyperspaces/
- *
- *  Created by Tony Arnold on 30/06/09.
- *  Copyright 2009 The CocoaBots. All rights reserved.
- *
- */
+//
+// NSApplication+CBAdditions.m
+// Hyperspaces
+//
+// Created by Tony Arnold on 30/06/09.
+// Copyright 2009 The CocoaBots. All rights reserved.
+//
 
 #import "NSApplication+CBAdditions.h"
 #import <Carbon/Carbon.h>
 
+NSString *const kShowDockIconUserDefaultsKey = @"ShowDockIcon";
 
 @implementation NSApplication (CBAdditions)
 
 + (id)infoValueForKey:(NSString *)key {
-	if ([[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:key]) {
-		return [[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:key];
-	}
+    if ([[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:key]) {
+        return [[[NSBundle mainBundle] localizedInfoDictionary] objectForKey:key];
+    }
 
-	return [[[NSBundle mainBundle] infoDictionary] objectForKey:key];
+    return [[[NSBundle mainBundle] infoDictionary] objectForKey:key];
 }
 
 - (BOOL)showsDockIcon {
-	return [[NSUserDefaults standardUserDefaults] boolForKey:HSShowDockIconUserDefaultsKey];
+    return [[NSUserDefaults standardUserDefaults] boolForKey:kShowDockIconUserDefaultsKey];
 }
 
 - (void)setShowsDockIcon:(BOOL)flag {
-	/* this should be called from the application delegate's applicationDidFinishLaunching
-	 * method or from some controller object's awakeFromNib method
-	 * Neat dockless hack using Carbon from <a href="http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it" title="http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it">http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-...</a>
-	 */
-	if (flag == YES) {
-		ProcessSerialNumber psn = { 0, kCurrentProcess };
-		/* display dock icon */
-		TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-		/* enable menu bar */
-		SetSystemUIMode(kUIModeNormal, 0);
-		/* switch to Dock.app */
-		[[NSWorkspace sharedWorkspace] launchAppWithBundleIdentifier:@"com.apple.dock" options:NSWorkspaceLaunchDefault additionalEventParamDescriptor:nil launchIdentifier:nil];
-		/* switch back */
-		[[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
-	}
+    // this should be called from the application delegate's applicationDidFinishLaunching method or from some controller object's awakeFromNib method neat dockless hack using Carbon from <a href="http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it" title="http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-to-do-it">http://codesorcery.net/2008/02/06/feature-requests-versus-the-right-way-...</a>
+    if (flag) {
+        ProcessSerialNumber psn = { 0, kCurrentProcess };
+        // display dock icon
+        TransformProcessType(&psn, kProcessTransformToForegroundApplication);
+        // enable menu bar
+        SetSystemUIMode(kUIModeNormal, 0);
+
+        // switch to Dock.app
+        if ([[NSWorkspace sharedWorkspace] launchAppWithBundleIdentifier:@"com.apple.dock" options:NSWorkspaceLaunchDefault additionalEventParamDescriptor:nil launchIdentifier:nil] == NO) {
+            CBLog(@"Could not launch application with identifier 'com.apple.dock'.");
+        }
+
+        // switch back
+        [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
+    }
 }
 
 - (void)relaunch {
-	NSString *executablePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"RelaunchTool" ofType:@""];
+    NSString *executablePath = [[NSBundle mainBundle] pathForResource:@"RelaunchTool" ofType:@""];
 
-	[NSTask launchedTaskWithLaunchPath:executablePath arguments:[NSArray arrayWithObjects:[[NSBundle mainBundle] bundlePath], [NSString stringWithFormat:@"%d", [[NSProcessInfo processInfo] processIdentifier]], nil]];
-	[NSApp terminate:self];
+    [NSTask launchedTaskWithLaunchPath:executablePath arguments:[NSArray arrayWithObjects:[[NSBundle mainBundle] bundlePath], [NSString stringWithFormat:@"%d", [[NSProcessInfo processInfo] processIdentifier]], nil]];
+    [NSApp terminate:self];
 }
 
-+ (void)getSystemVersionMajor:(unsigned *)major minor:(unsigned *)minor bugFix:(unsigned *)bugFix {
-	OSErr  err;
-	SInt32 systemVersion, versionMajor, versionMinor, versionBugFix;
++ (BOOL)systemIsLeopard {
+    return (floor(NSAppKitVersionNumber) <= NSAppKitVersionNumber10_5);
+}
 
-	if ((err = Gestalt(gestaltSystemVersion, &systemVersion)) != noErr) {
-		goto fail;
-	}
++ (BOOL)systemIsLeopardOrHigher {
+    return floor(NSAppKitVersionNumber) >= NSAppKitVersionNumber10_5;
+}
 
-	if (systemVersion < 0x1040) {
-		if (major) {
-			*major = ((systemVersion & 0xF000) >> 12) * 10 + ((systemVersion & 0x0F00) >> 8);
-		}
++ (BOOL)systemIsSnowLeopard {
+    return (floor(NSAppKitVersionNumber) <= NSAppKitVersionNumber10_6);
+}
 
-		if (minor) {
-			*minor = (systemVersion & 0x00F0) >> 4;
-		}
++ (BOOL)systemIsSnowLeopardOrHigher {
+    return floor(NSAppKitVersionNumber) >= NSAppKitVersionNumber10_6;
+}
 
-		if (bugFix) {
-			*bugFix = (systemVersion & 0x000F);
-		}
-	} else {
-		if ((err = Gestalt(gestaltSystemVersionMajor, &versionMajor)) != noErr) {
-			goto fail;
-		}
++ (BOOL)systemIsLion {
+    return floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_6;
+}
 
-		if ((err = Gestalt(gestaltSystemVersionMinor, &versionMinor)) != noErr) {
-			goto fail;
-		}
-
-		if ((err = Gestalt(gestaltSystemVersionBugFix, &versionBugFix)) != noErr) {
-			goto fail;
-		}
-
-		if (major) {
-			*major = versionMajor;
-		}
-
-		if (minor) {
-			*minor = versionMinor;
-		}
-
-		if (bugFix) {
-			*bugFix = versionBugFix;
-		}
-	}
-
-	return;
-
- fail:
-	NSLog(@"Unable to obtain system version: %ld", (long)err);
-
-	if (major) {
-		*major = 10;
-	}
-
-	if (minor) {
-		*minor = 0;
-	}
-
-	if (bugFix) {
-		*bugFix = 0;
-	}
+- (NSString *)applicationCopyrightInformation {
+    return [NSApplication infoValueForKey:@"NSHumanReadableCopyright"];
 }
 
 - (NSString *)applicationName {
-	return [NSApplication infoValueForKey:@"CFBundleExecutable"];
+    return [NSApplication infoValueForKey:@"CFBundleExecutable"];
 }
 
 - (NSString *)applicationShortVersionString {
-	return [NSApplication infoValueForKey:@"CFBundleShortVersionString"];
+    return [NSApplication infoValueForKey:@"CFBundleShortVersionString"];
 }
 
 - (NSString *)applicationVersion {
-	return [NSApplication infoValueForKey:@"CFBundleVersion"];
+    return [NSApplication infoValueForKey:@"CFBundleVersion"];
+}
+
+- (NSString *)applicationIdentifier {
+    return [NSApplication infoValueForKey:@"CFBundleIdentifier"];
 }
 
 @end
